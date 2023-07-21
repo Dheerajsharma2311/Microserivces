ff433fa5d1200b7b28385519c57d6057980a9376
Dheerajsharma2311 <68056149+Dheerajsharma2311@users.noreply.github.com>
2023-07-21 11:39:16 +0530

    Add files via upload

---
+++ API-Gateway/src/main/java/com/hdfc/APIGateway/FallBackMethodController.java
@@ -0,0 +1,20 @@
+package com.hdfc.APIGateway;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallBackMethodController {
+
+    @GetMapping("/DecryptServiceFallBack")
+    public String decryptServiceFallBackMethod() {
+        return "Decrypt Service is taking longer than Expected." +
+                " Please try again later";
+    }
+
+    @GetMapping("/EncryptServiceFallBack")
+    public String departmentServiceFallBackMethod() {
+        return "Encrypt Service is taking longer than Expected." +
+                " Please try again later";
+    }
+}
---
+++ API-Gateway/src/test/java/com/hdfc/APIGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdfc.APIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
